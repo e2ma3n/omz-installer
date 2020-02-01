0f449bc7952feed6bbfa36d7a52059341eda0567
Iman Homayouni <e2ma3n@gmail.com>
2020-02-01 18:15:26 +0330

    Update omz-installer.sh

--- omz-installer.sh
+++ omz-installer.sh
@@ -8,17 +8,54 @@
 # Oh My Zsh Installer v7.0 - [Install Oh My Zsh automatically]
 #--------------------------------------------------------------#
 
-apt-get update
-apt-get -y dist-upgrade
-apt install -y zsh powerline fonts-powerline
+function install {
+    ping -c 1 github.com &> /dev/null
+    if [ "$?" = "0" ] ; then
+        rm -rf ~/.oh-my-zsh &> /dev/null
+        echo "[>] git clone oh-my-zsh.git"
+        git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+        cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+        sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
+        chsh -s /bin/zsh
+        rm -rf $HOME/.zsh-syntax-highlighting &> /dev/null
+        echo "[>] git clone zsh-syntax-highlighting.git"
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+        echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+        echo "[>] Done"
+        . $HOME/.zshrc
+    else
+        echo "[!] Sorry, can not connect to github.com"
+    fi
+}
+
+which sudo &> /dev/null
 if [ "$?" = "0" ] ; then
-    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-    cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-    sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
-    chsh -s /bin/zsh
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
-    echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+    sudo -v &> /dev/null
+    if [ "$?" = "0" ] ; then
+        sudo apt-get update
+        sudo apt-get -y dist-upgrade
+        sudo apt install -y zsh powerline fonts-powerline
+        if [ "$?" = "0" ] ; then
+            install
+        else
+            echo '[!] error in update and upgrade system'
+            exit 1
+        fi
+    else
+        echo "[!] Sorry, your user may not run sudo on system."
+    fi
 else
-    echo '[>] error in update and upgrade system'
-    exit 1
+    if [ "$UID" = "0" ] ; then
+        apt-get update
+        apt-get -y dist-upgrade
+        apt install -y zsh powerline fonts-powerline
+        if [ "$?" = "0" ] ; then
+            install
+        else
+            echo '[!] error in update and upgrade system'
+            exit 1
+        fi
+    else
+        echo "[!] Sorry, you must run root user."
+    fi
 fi
